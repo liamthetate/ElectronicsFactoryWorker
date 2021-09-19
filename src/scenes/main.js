2614e43b1fb81484a5b7aedda4e649b00ac78ada
Liam Tate <mrtea@protonmail.com>
2021-09-19 14:07:49 +0100

    lots of stuff lol, mechanics, maps, ui

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -8,6 +8,8 @@ const k = kaboom({
 	scale: 1,
 	debug: true,
 	clearColor: [0,0,0,1],
+	width: 480,
+  	height: 480,
 })
 
 loadRoot('sprites/Zelda/')
@@ -35,6 +37,7 @@ loadSprite('bottom-door', '7.png')
 loadSprite('kaboom', '9.png')
 loadSprite('widget', '20.png')
 loadSprite('worker', '12a.png')
+loadSprite('worker-up', '16a.png')
 loadSprite('hole', '8.png')
 
 scene('game',({ level, score }) => {
@@ -42,29 +45,61 @@ scene('game',({ level, score }) => {
 		layers(['bkg','obj', 'top', 'ui'], 'obj')
 
 		const maps = [
-			[
+			[//simulator mode
+				'yccccccccw',
+				')________(',
+				'aqqqqqqqqb',
+				'a        b',
+				'%        }',
+				'a        *',
+				'a        b',
+				'apppp pppb',
+				'(________)',
+				'xddddddddz',
+			],
+
+			[//simulator mode
+				'yccc  cccw',
+				'a        b',
+				'a        b',
+				'a        b',
+				'a        b',
+				'a        b',
+				'a        b',
+				'apppp pppb',
+				'(________)',
+				'xddddddddz',
+			],
+
+			[//simulator mode
 				'yccc  cccw',
 				'a p    p b',
-				'a___  ___b',
+				'a__    __b',
 				'a p    p b',
-				'a___  ___b',
+				'a__    __b',
 				'a p    p b',
-				'a___  ___b',
+				'a__    __b',
+				'appp  pppb',
+				'(________)',
+				'xddddddddz',
+			],
+
+			[//fun mode
+				'yccc  cccw',
+				'a p    p b',
+				'a__o  o__b',
+				'a p    p b',
+				'a__o  o__b',
+				'a p    p b',
+				'a__o  o__b',
 				'a        b',
 				'(________)',
 				'xddddddddz',
 			],
 
-			[
-				'yccc  cccw',
-				'a p    p b',
-				'a__o  o__b',
-				'a p    p b',
-				'a__o  o__b',
-				'a p    p b',
-				'a__o  o__b',
-				'a        b',
-				'(________)',
+			[//hallway
+				'ycccc^cccw',
+				'a         ',
 				'xddddddddz',
 			],
 		]
@@ -80,16 +115,17 @@ scene('game',({ level, score }) => {
 			'x' : [sprite('bottom-left-wall'), 'wall', solid()],
 			'y' : [sprite('top-left-wall'), 'wall', solid()],
 			'z' : [sprite('bottom-right-wall'), 'wall', solid()],
-			'%' : [sprite('left-door')],
+			'%' : [sprite('left-door', 'hallway')],
 			'^' : [sprite('top-door'), 'wall', 'next-level'],
 			'$' : [sprite('stairs'), 'next-level'],
 			'*' : [sprite('slicer'), 'slicer', 'dangerous', {dir: -1}], //{} = direction
-			'}' : [sprite('skeletor'), 'skeletor', 'dangerous', {dir: -1, timer: -1}],
+			'}' : [sprite('skeletor'), solid(), 'skeletor', 'dangerous', {dir: -1, timer: -1}],
 			')' : [sprite('lanterns'), 'wall', 'not-done', solid()],
 			'(' : [sprite('firepot'), solid(), layer('ui')],
 			'_' : [sprite('bottom-door'), solid(), 'wall'],
 			'p' : [sprite('worker'), solid(), 'worker'],
-			'o' : [sprite('hole'), solid(), 'hole']
+			'q' : [sprite('worker-up'), solid(), 'worker'],
+			'o' : [sprite('hole'), solid(), 'hole'],
 		}
 
 		addLevel(maps[level], levelCfg)
@@ -109,11 +145,12 @@ scene('game',({ level, score }) => {
 
 		//PLAYER
 		const player = add([
-			sprite('link-going-right'),
-			pos(200,200),
+			sprite('link-going-down'),
+			pos(225,200), ///pos(225,70),
 			{
-				dir: vec2(1,0)
+				dir: vec2(0,1)
 			}
+			
 		])
 
 		player.action(()=> {
@@ -137,25 +174,65 @@ scene('game',({ level, score }) => {
 		// 	})
 		// })
 
+
+		// if (WIDGET_SPEED < 60) {
+		// 	GAP_BETWEEN_WIDGETS = 1
+		// }
+
+		// else if (WIDGET_SPEED < 90) {
+		// 	GAP_BETWEEN_WIDGETS = 0.9
+		// }
+
+		// else if (WIDGET_SPEED < 120) {
+		// 	GAP_BETWEEN_WIDGETS = 0.6
+		// }
+
+		// else if (WIDGET_SPEED < 150) {
+		// 	GAP_BETWEEN_WIDGETS = 0.4
+		// }
+
+		// else if (WIDGET_SPEED < 210) {
+		// 	GAP_BETWEEN_WIDGETS = 0.3
+		// } 
+		
+		// else if (WIDGET_SPEED < 300) {
+		// 	GAP_BETWEEN_WIDGETS = 0.3
+		// } 
+
 		//WIDGET
-		const WIDGET_SPEED = 30
-
-		// PLAYER PICKS UP WIDGET
-		collides('hole', 'kaboom', (k) => {
-			scoreLabel.text = 'got widget'
-		})
-
-		// PLAYER SUCCESSFULLY FITS COMPONANT
-		collides('kaboom', 'widget', (k,w) => {
+		let WIDGET_SPEED = 35
+		let GAP_BETWEEN_WIDGETS = 2
+		
+		//SPEEDS UP
+		collides('kaboom', 'skeletor', (k) => {
+			camShake(1)
 			wait(1, () => {
 				destroy(k)
 			})
-			destroy(w)
-			scoreLabel.value++
-			scoreLabel.text = scoreLabel.value
-			dailyTarget.value--
-			dailyTarget.text = dailyTarget.value
+			WIDGET_SPEED += 30
+			GAP_BETWEEN_WIDGETS -= 0.3
+			if (GAP_BETWEEN_WIDGETS < 0.1) {
+				GAP_BETWEEN_WIDGETS = 0.1
+			}
+			theMachineLoop()			
 		})
+
+		//SLOWS DOWN
+		collides('kaboom', 'slicer', (k) => {
+			camShake(1)
+			wait(1, () => {
+				destroy(k)
+			})
+			WIDGET_SPEED -= 30
+			// if (WIDGET_SPEED < 30) {
+			// 	WIDGET_SPEED = 30
+			// }
+			GAP_BETWEEN_WIDGETS += 0.3
+			// if (GAP_BETWEEN_WIDGETS > 2) {
+			// 	GAP_BETWEEN_WIDGETS = 2
+			// }
+		})
+
 
 		//WIDGET SPAWNER
 		function spawnWidgets () {
@@ -163,22 +240,76 @@ scene('game',({ level, score }) => {
 			obj.action( () => {
 				obj.move(WIDGET_SPEED,0)
 			})
-			wait(23, () => {
-				destroy(obj)
-			})
+			// wait(23, () => {
+			// 	destroy(obj)
+			// })
 		}
 
-		wait(1, () => {
-			loop(3, () => {
+		// gaps
+		function theMachineLoop () {
+			wait(1, ()=> {
+				loop(GAP_BETWEEN_WIDGETS, () => { //n = gap between widgets
 				spawnWidgets()
+				npcSpawnWidgets() // NPC
+				})
+			})
+	 	}
+
+		theMachineLoop()
+		
+
+		//NPC WIDGET SPAWNER
+		function npcSpawnWidgets () {
+			const obj = add([sprite('hole'), pos(450,48), 'hole'])
+			obj.action( () => {
+				obj.move(-WIDGET_SPEED,0)
+			})
+			// wait(23, () => {
+			// 	destroy(obj)
+			// })
+		}
+
+		
+
+
+
+		// // PLAYER PICKS UP WIDGET
+		// collides('firepot', 'kaboom', (k) => {
+		// 	scoreLabel.text = 'got widget'
+		// })
+
+		// PLAYER SUCCESSFULLY FITS COMPONANT
+		collides('kaboom', 'widget', (k,w) => {
+			wait(1, () => {
+				destroy(k)
+			})
+			destroy(w)
+
+			//update score & target
+			scoreLabel.value++
+			scoreLabel.text = scoreLabel.value
+			dailyTarget.value--
+			dailyTarget.text = dailyTarget.value
+
+			// show new graphic for fitting widget
+			const obj = add([sprite('hole'), pos(240,384), 'hole'])
+			obj.action( () => {
+				obj.move(WIDGET_SPEED,0)
+			})
+			wait(10, () => {
+				destroy(obj)
 			})
 		})
 
-		// BAD WORKER!
+		
+		
+		
+
+		// MISSED ONE - BAD WORKER!
 		collides('widget', 'not-done', (w) => {
 			camShake(10)
 			destroy(w)
-			const message = add([text('FAIL!'), pos(300, 350), scale(5)])
+			const message = add([text('FAIL!'), pos(270, 390), scale(5)])
 			scoreLabel.value--
 			scoreLabel.text = scoreLabel.value
 			dailyTarget.value++
@@ -186,6 +317,10 @@ scene('game',({ level, score }) => {
 			wait(1, ()=> {
 				destroy(message)
 			})
+		})
+
+		collides('hole', 'not-done', (h) => {
+			destroy(h)
 		})
 
 	
@@ -228,6 +363,68 @@ scene('game',({ level, score }) => {
 			spawnKaboom(player.pos.add(player.dir.scale(48)))
 		})
 
+		// WORKER INTERACTION
+		const workerMessage = [
+			['I am so happy'], //0
+			['Work = meaning'], //1
+			['Unionise?!'], //2
+			['zzZ'], //3
+			['Best time ever!'], //4
+			["One day I'll be rich!"], //5
+			["I'm productive!"], //6
+			["Busy busy busy!"], //7
+			["Steve is my hero!"], //8
+			["I love job creators"], //9
+			["Work hard!"], //10
+			["I am disciplined"], //11
+			["Stay focused!"], //12
+			["You can achieve anything!"], //13
+			["I'm a hard worker!"], //14
+			["I'm my best self!"], //15
+			["YOLO!"], //16
+		]
+
+		// mouse click = conversations
+		clicks('worker', () => {
+			const conversation = add([
+				text(workerMessage[parseInt(rand(16))]),
+				scale(2),
+				pos(250,250),
+				origin('center'),
+			])
+
+			wait(1, () => {
+				destroy(conversation)
+			})
+		})
+
+
+
+		// // space bar = conversations
+		// collides('kaboom', 'worker', (k) => {
+		// 	destroy(k)
+		// 	const conversation = add([
+		// 		text(workerMessage[parseInt(rand(6))]),
+		// 		scale(2),
+		// 		pos(250,250),
+		// 		origin('center'),
+		// 	])
+		// 	wait(1, () => {
+		// 		destroy(conversation)
+		// 	})
+			
+		// })
+
+		// mouse click = conversations
+		clicks('widget', () => {
+			spawnKaboom(player.pos.add(player.dir.scale(48)))
+		})
+
+
+
+/
+
+
 		// //ENEMY
 		// const ENEMY_SPEED = 120
 		// action('slicer', (s) => {
@@ -262,38 +459,9 @@ scene('game',({ level, score }) => {
 		// })
 
 
-		// WORKER INTERACTION
-		const workerMessage = [
-			['I am so happy'],
-			['Work is good!'],
-			['Only 1679 to go!'],
-			['zzZ'],
-			['Unionise? LOL JOKES!'],
-			['Get back to work :)'],
-			['Best time ever!'],
-		]
+		
 
-		// mouse click destroyrs things with the tags
-		clicks('worker', () => {
-			const conversation = add([
-				text(workerMessage[parseInt(rand(6))]),
-				scale(2),
-				pos(mousePos()),
-			])
-			wait(2, () => {
-				destroy(conversation)
-			})
-		})
 
-		// end screen
-		scene('lose', ({score}) => {
-			add([
-				text(score),
-				pos(width() /2, height() /2),
-				origin('center'),
-				scale(10)
-			])
-		})
 
 
 
@@ -318,8 +486,37 @@ scene('game',({ level, score }) => {
 
 		
 
+		
+		// const TIME_LEFT = 43200
+
+		// add([
+		// 	text('SHIFT ENDS:'),
+		// 	pos(380, 10),
+		// 	scale(1),
+		// ])
+
+		// const timer = add([
+		// 	text('0'),
+		// 	pos(380, 25),
+		// 	scale(2),
+		// 	layer('ui'),
+		// 	{
+		// 		time: TIME_LEFT
+		// 	},
+		// ])
+		
+		// /* calls on the timer const, then drills into it */
+		// timer.action(() => { /* action is called every frame */
+		// 	timer.time -= dt() /* delta time since last frame */
+		// 	timer.text = timer.time.toFixed(0)
+		// 	if (timer.time <= 0) {
+		// 		go('bedtime', {score: score.value}) /*go to lose scene and take score with you*/
+		// 	}
+		// })
+
+
 		//SHIFT TIME
-		const TIME_LEFT = 43200
+		const ACTUAL_TIME_LEFT = 43200
 
 		add([
 			text('SHIFT ENDS:'),
@@ -327,29 +524,77 @@ scene('game',({ level, score }) => {
 			scale(1),
 		])
 
-		const timer = add([
+		const actualTime = add([
 			text('0'),
 			pos(380, 25),
 			scale(2),
 			layer('ui'),
 			{
-				time: TIME_LEFT
+				time: ACTUAL_TIME_LEFT,
 			},
 		])
 		
 		/* calls on the timer const, then drills into it */
-		timer.action(() => { /* action is called every frame */
-			timer.time -= dt() /* delta time since last frame */
-			timer.text = timer.time.toFixed(0)
-			if (timer.time <= 0) {
-				go('bedtime', {score: score.value}) /*go to lose scene and take score with you*/
-			}
+		actualTime.action(() => { /* action is called every frame */
+			actualTime.time -= dt() /* delta time since last frame */
+			const totalSeconds = actualTime.time
+			actualTime.text = totalSeconds.toFixed(0)
 		})
 
+
+
 		
+		
+		//3600secs in an hour
+		
+		// const hourTimer = add([
+		// 	text('0'),
+		// 	pos(380, 75),
+		// 	scale(2),
+		// 	layer('ui'),
+		// ])
 
-})
+		// const minTimer = add([
+		// 	text('0'),
+		// 	pos(420, 75),
+		// 	scale(2),
+		// 	layer('ui'),
+		// ])
+
+		// const secTimer = add([
+		// 	text('0'),
+		// 	pos(460, 75),
+		// 	scale(2),
+		// 	layer('ui'),
+		// ])
+
+		// hourTimer.action(() => { 
+		// 	let sec_value = ACTUAL_TIME_LEFT % (24 * 3600)
+		// 	let hour_value = sec_value / 3600
+		// 	sec_value %= 3600
+		// 	let min_value = sec_value / 60
+		// 	sec_value %= 60
+		// 	hourTimer.text = hour_value
+		// 	minTimer.text = min_value
+
+		// 	sec_value += dt()
+		// 	secTimer.text = sec_value.toFixed(0)
+		// })
 
 
 
-start('game', { level: 0, score: 0 })
+		// end screen
+		scene('lose', ({score}) => {
+			add([
+				text(score),
+				pos(width() /2, height() /2),
+				origin('center'),
+				scale(10)
+			])
+		})
+
+	})
+
+
+
+	start('game', { level: 0, score: 0 })
